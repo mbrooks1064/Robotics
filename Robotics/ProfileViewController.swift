246acd56d4db0f044e1c2fc07f686d83fb611249
period3 <period3@p-162mba-01.d214.org>
2018-10-29 10:27:35 -0500

    I deleted didReceiveMemoryWarning, and added 2nd view controller, and added to storyboard

--- Robotics/ProfileViewController.swift
+++ Robotics/ProfileViewController.swift
@@ -1,5 +1,5 @@
 //
-//  ViewController.swift
+//  ProfileViewController.swift
 //  Robotics
 //
 //  Created by period3 on 10/29/18.
@@ -8,15 +8,14 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ProfileViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
+
 
     }
 
     
 
-
 }
-
