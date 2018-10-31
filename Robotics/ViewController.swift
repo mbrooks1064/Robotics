9128465a6d7ea81a5f38e1d4fa712f060f0cdc63
period3 <period3@p-162mba-01.d214.org>
2018-10-31 10:50:06 -0500

    Updated button Outlets and stuff

--- Robotics/ViewController.swift
+++ Robotics/ViewController.swift
@@ -9,10 +9,30 @@
 import UIKit
 
 class ViewController: UIViewController {
+    @IBOutlet weak var teamOne: UIButton!
+    @IBOutlet weak var teamTwo: UIButton!
+    @IBOutlet weak var teamThree: UIButton!
+    @IBOutlet weak var teamFour: UIButton!
+    @IBOutlet weak var teamFive: UIButton!
+    @IBOutlet weak var teamSix: UIButton!
+    @IBOutlet weak var teamSeven: UIButton!
+    @IBOutlet weak var teamEight: UIButton!
+    @IBOutlet weak var teamNine: UIButton!
     var adminAccess = false
+    var teams: [String] = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+//        teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
+        teamOne.titleLabel?.text = teams[0]
+        teamTwo.titleLabel?.text = teams[1]
+        teamThree.titleLabel?.text = teams[2]
+        teamFour.titleLabel?.text = teams[3]
+        teamFive.titleLabel?.text = teams[4]
+        teamSix.titleLabel?.text = teams[5]
+        teamSeven.titleLabel?.text = teams[6]
+        teamEight.titleLabel?.text = teams[7]
+        teamNine.titleLabel?.text = teams[8]
+        print(teamNine.titleLabel?.text)
     }
     @IBAction func adminButton(_ sender: UIBarButtonItem) {
     alert()
