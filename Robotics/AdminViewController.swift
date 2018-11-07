e9effd766cfbfe8c50290b823c9f5fc021a8a024
period3 <period3@p-162mba-01.d214.org>
2018-11-07 10:19:02 -0600

    Nothing

--- Robotics/AdminViewController.swift
+++ Robotics/AdminViewController.swift
@@ -25,6 +25,7 @@ class AdminViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
         teamArray.remove(at: pickerSpot)
         teamArray.insert(teamNameTextField.text!, at: pickerSpot)
         imagePicker.reloadAllComponents()
+        self.performSegue(withIdentifier: "Unwind", sender: self)
 
     }
     
@@ -42,6 +43,7 @@ class AdminViewController: UIViewController, UIPickerViewDelegate, UIPickerViewD
     override func unwind(for unwindSegue: UIStoryboardSegue, towardsViewController subsequentVC: UIViewController) {
         let nvc = unwindSegue.destination as! ViewController
         nvc.teams = teamArray
+        print(nvc.teams)
     }
     
 }
