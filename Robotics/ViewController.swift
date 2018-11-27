ec703c265af6b6a6d7896a1c6c8f36f80575dd7f
period3 <period3@p-162mba-01.local>
2018-11-27 10:36:26 -0600

    Started/attempted to start saving data

--- Robotics/ViewController.swift
+++ Robotics/ViewController.swift
@@ -19,6 +19,7 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     @IBOutlet weak var teamSeven: UIButton!
     @IBOutlet weak var teamEight: UIButton!
     @IBOutlet weak var teamNine: UIButton!
+//    var firstTimeOpened = true
     var initiatedSegue: String!
     var teamNumber: Int!
     var adminAccess = false
@@ -26,9 +27,24 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
     var alertTitle = "On Deck Next:"
     var ref: DatabaseReference!
     var bioArray: [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9"]
-    var teams: [String] = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
+    let defaults = UserDefaults.standard
+    var teams = [String]()
+    var team = String()
+    {
+        didSet
+        {
+                        self.defaults.set(teams, forKey: team)
+        }
+    }
     override func viewWillAppear(_ animated: Bool) {
         super.viewWillAppear(true)
+//        if firstTimeOpened == true {
+//            firstTimeOpened = false
+//            teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
+//        }
+        if let saveData = defaults.object(forKey: team) as? [String] {
+            teams = saveData
+        }
         ref = Database.database().reference()
         
         //        teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
@@ -45,28 +61,32 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         navigationController?.delegate = self
         
     }
-//    override func viewDidLoad() {
-//        super.viewDidLoad()
-//        ref = Database.database().reference()
-//
-////        teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
-//        teamOne.setTitle(teams[0],for: .normal)
-//        teamTwo.setTitle(teams[1],for: .normal)
-//        teamThree.setTitle(teams[2],for: .normal)
-//        teamFour.setTitle(teams[3],for: .normal)
-//        teamFive.setTitle(teams[4],for: .normal)
-//        teamSix.setTitle(teams[5],for: .normal)
-//        teamSeven.setTitle(teams[6],for: .normal)
-//        teamEight.setTitle(teams[7],for: .normal)
-//        teamNine.setTitle(teams[8],for: .normal)
-//
-//        navigationController?.delegate = self
-//
-//    }
+    override func viewDidLoad() {
+        super.viewDidLoad()
+teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
+    }
+    //    override func viewDidLoad() {
+    //        super.viewDidLoad()
+    //        ref = Database.database().reference()
+    //
+    ////        teams = ["Team 1", "Team 2", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
+    //        teamOne.setTitle(teams[0],for: .normal)
+    //        teamTwo.setTitle(teams[1],for: .normal)
+    //        teamThree.setTitle(teams[2],for: .normal)
+    //        teamFour.setTitle(teams[3],for: .normal)
+    //        teamFive.setTitle(teams[4],for: .normal)
+    //        teamSix.setTitle(teams[5],for: .normal)
+    //        teamSeven.setTitle(teams[6],for: .normal)
+    //        teamEight.setTitle(teams[7],for: .normal)
+    //        teamNine.setTitle(teams[8],for: .normal)
+    //
+    //        navigationController?.delegate = self
+    //
+    //    }
     @IBAction func adminButton(_ sender: UIBarButtonItem) {
         if adminAccess == true {
             self.performSegue(withIdentifier: "toAdmin", sender: self)
-
+            
         } else {
             alert()
             
@@ -82,14 +102,14 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
                 self.adminAccess = true
             }
             
-    
+            
         }
         
         alert.addTextField { (textField) in
-
+            
             textField.placeholder = "Password"
             
-            }
+        }
         let cancelAction = UIAlertAction(title: "Cancel", style: .cancel, handler: nil)
         alert.addAction(okAction)
         alert.addAction(cancelAction)
@@ -99,63 +119,63 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
         initiatedSegue = teams[0]
         teamNumber = 1
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamTwo(_ sender: UIButton) {
         initiatedSegue = teams[1]
         teamNumber = 2
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamThree(_ sender: UIButton) {
         initiatedSegue = teams[2]
         teamNumber = 3
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamFour(_ sender: UIButton) {
         initiatedSegue = teams[3]
         teamNumber = 4
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamFive(_ sender: UIButton) {
         initiatedSegue = teams[4]
         teamNumber = 5
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamSix(_ sender: UIButton) {
         initiatedSegue = teams[5]
         teamNumber = 6
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamSeven(_ sender: UIButton) {
         initiatedSegue = teams[6]
         teamNumber = 7
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamEight(_ sender: UIButton) {
         initiatedSegue = teams[7]
         teamNumber = 8
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     @IBAction func teamNine(_ sender: UIButton) {
         initiatedSegue = teams[8]
         teamNumber = 9
-
+        
         self.performSegue(withIdentifier: "segueFromBracket", sender: self)
-
+        
     }
     override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
         if segue.identifier == "toAdmin" {
@@ -163,19 +183,19 @@ class ViewController: UIViewController, UINavigationControllerDelegate {
             n2vc.teamThatWasTapped = initiatedSegue
             n2vc.teamArray = teams
             n2vc.bioArrayAdmin = bioArray
-//            n2vc.receiveTeamNumber = teamNumber
+            //            n2vc.receiveTeamNumber = teamNumber
         } else {
             let nvc = segue.destination as! ProfileViewController
             nvc.teamThatWasTapped = initiatedSegue
             nvc.teamArray = teams
             nvc.receiveTeamNumber = teamNumber
             nvc.bioArrayProfile = bioArray
-
+            
         }
         
     }
-
-        @IBAction func unwindToOne(_ sender: UIStoryboardSegue) {}
+    
+    @IBAction func unwindToOne(_ sender: UIStoryboardSegue) {}
     override func viewDidAppear(_ animated: Bool) {
         super.viewDidAppear(animated)
         ref.child("condition").observe(.value) { (snap: DataSnapshot) in
