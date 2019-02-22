4453d090f9115b4990183648499e02dec373e0e9
Brooks <period3@p-162mba-01.d214.org>
2019-02-22 07:34:31 -0600

    Forgot some

--- Robotics/ProfileViewController.swift
+++ Robotics/ProfileViewController.swift
@@ -18,11 +18,14 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, S
     @IBOutlet weak var bioText: UITextView!
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var pickerView: UIPickerView!
-    var bioArrayProfile: [String] = ["Bobby's cool team", "queer team 2", "3", "4", "5", "6", "7", "8", "9"]
-    var robotNameProfile: [String] = ["Mini Bobby", "a'i", "4138kslkdfe", "oij23jrinkd", "weoinz", "apowijef", "';alkclel", "..z.e.e..e", "alseje",]
-    var teamNamesProfile: [String] = ["Bobby's Boiz", "as;ofjd", "Team 3", "Team 4", "Team 5", "Team 6", "Team 7", "Team 8", "Team 9"]
-    var teamMembersProfile: [String] = ["Bobby and others", "a'silfj", "as'lfij", "awefa", "asfdfc", "asdfwe", "123mvik", "asdijfoeij", "aiweojfmxsz"]
-    var pictureOfRobotsProfile: [UIImage] = [#imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture"), #imageLiteral(resourceName: "NoPicture")]
+    var bioArrayProfile: [String] = ["Bobby's cool team", "", "Hard working, nerds", "Flagged for Content", "A guy who was bored, the bored guy's brother, a sophomore who was lazy, and their crazy creation", "", "7", "8", "9", "Three seniors who love robots and wanted to avenge last years bot Baby Ajax", "11", "12", "13"]
+    var robotNameProfile: [String] = ["Mini Bobby", "Chopper", "Bowser Jr", "The Killer Stop Sign", "The Road Spike", "Trilobite", "Hurricane", "Scorpion", "Rhino", "Very Vicious Mechanical Fiend", "11", "12", "13"]
+    var teamNamesProfile: [String] = ["Bobby's Boiz", "Mustangs", "Wild Pack", "The Polacy", "The Rayburns + 1", "Team 6", "Team 7", "Team 8", "Team 9", "Palatine Robotics", "11", "12", "13", "14"]
+    var teamMembersProfile: [String] = ["Bobby and others", "Brian Sroka, Tristan Anderson, Jacob Gustafson, Mebin George, Nate Morgan", "Michal Wantuch, George Fousekis, Johnny O'Grady", "Daniel Sobas Eryk Kozuch", "Shane, Brian, and Kahan", "Sean Pandaleon, Trever Lundvick, Adam Chartouni", "Boris Jevtic, Michael Szydlo, Jack Ingemunson, Chris Paraskevopoulos, Quinn Robinson, Elliot Toljanic, Alex Lazes", "Matt Tanny, Renee Ramirez, Oscar Quintero, Jaquelin Z, Luis Vega , Martin Davis", "Jack Sroka, Joey Emmerson, Kaeleigh Wilson, Jeremy Arroyo, Jacob Knaack", "Malcolm Filichia, Grace Huff, Keely McEnery", "11", "12", "13", "14"]
+    var pictureOfRobotsProfile: [UIImage] = [#imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "Team2"), #imageLiteral(resourceName: "Team3"), #imageLiteral(resourceName: "Team4"), #imageLiteral(resourceName: "Team5"), #imageLiteral(resourceName: "Team6"), #imageLiteral(resourceName: "Team7"), #imageLiteral(resourceName: "Team8"), #imageLiteral(resourceName: "Team9"), #imageLiteral(resourceName: "Team10"), #imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "RoboRumble"), #imageLiteral(resourceName: "RoboRumble")]
+    var schoolNameTeamName: [String] = ["1", "2", "3", "4", "5", "6", "7", "8", "9", "10", "11", "12", "13", "14"]
+//    var pictureOfRobotsProfilFix: [UIImage] = [image("Team1"),]
+
     var pickerSpot: Int = 0
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -54,14 +57,20 @@ class ProfileViewController: UIViewController, UINavigationControllerDelegate, S
     func updateStuff(A: Int) {
         bioText.allowsEditingTextAttributes = false
         pickerSpot = pickerView.selectedRow(inComponent: 0)
-        bioText.text = bioArrayProfile[A]
+//        if biotext == "Flagged for Content" {
+//            bioText.text = bioArrayProfile[A]
+//            bioText.textColor = #colorLiteral(red: 1, green: 0.06974860209, blue: 0, alpha: 1)
+//        } else {
+            bioText.text = bioArrayProfile[A]
+//        }
         teamMembersLabel.text = teamMembersProfile[A]
         robotNameLabel.text = robotNameProfile[A]
+    
         imageView.image = pictureOfRobotsProfile[A]
         pickerView.reloadAllComponents()
     }
     func openLinkLivestream() {
-        let url = URL(string: "https://www.google.com/")!
+        let url = URL(string: "https://www.youtube.com/watch?v=MuWssKF0zmU")!
         //        UIApplication.shared.open(url!, options: [:], completionHandler: nil)
         let vc = SFSafariViewController(url: url)
         vc.delegate = self as! SFSafariViewControllerDelegate
